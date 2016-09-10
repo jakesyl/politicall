9b0894682832ff35eca6735e92c8f83ee77ed9bd
Jake Sylvestre <jakesyl@gmail.com>
2016-09-10 04:13:33 -0400

    add politcall calls metric

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -7,7 +7,16 @@
 <!-- AdminLTE App -->
 <script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
 
+<script src="plugins/chartjs/Chart.min.js"></script>
+
+<!-- SlimScroll 1.3.0 -->
+<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+
+<!-- FastClick -->
+<script src="plugins/fastclick/fastclick.js"></script>
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
       Both of these plugins are recommended to enhance the
       user experience. Slimscroll is required when using the
       fixed layout. -->
+
+

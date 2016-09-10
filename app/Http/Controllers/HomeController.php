2917aa2582c19b5c09a01a8c9e6e83db0d4a3dec
Jake Sylvestre <jakesyl@gmail.com>
2016-09-10 15:46:04 -0400

    add volunteers column

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,13 +34,35 @@ class HomeController extends Controller
      */
     public function index()
     {
-	$call = new CallController();
-	$aveLen = $call->getAverageCallLength();
-	$negative =DB::table('calls')->where("opinion","Negative")->count();
-	$positive =DB::table('calls')->where("opinion","Positive")->count();
-	$neutral =DB::table('calls')->where("opinion","Neutral")->count();
-	$pickupCount = DB::table('calls')->where('pickup',true)->count();
-	$total = DB::table('calls')->count();
-        return view('home',['negative'=>$negative,'positive'=>$positive,'neutral'=>$neutral,'pickup'=>$pickupCount,'total'=>$total,'aveLen'=>$aveLen]);
+      	$call = new CallController();
+      	$aveLen = $call->getAverageCallLength();
+      	$negative =DB::table('calls')
+          ->where("opinion","Negative")
+          ->count();
+      	$positive =DB::table('calls')
+          ->where("opinion","Positive")
+          ->count();
+      	$neutral =DB::table('calls')
+          ->where("opinion","Neutral")
+          ->count();
+      	$pickupCount = DB::table('calls')
+          ->where('pickup',true)
+          ->count();
+      	$total = DB::table('calls')
+          ->count();
+
+        $volunteers = DB::table('volunteers')
+          ->count();
+        return view('home',
+          [
+            'negative'=>$negative,
+            'positive'=>$positive,
+            'neutral'=>$neutral,
+            'pickup'=>$pickupCount,
+            'total'=>$total,
+            'aveLen'=>$aveLen,
+            'volunteerCount' => $volunteers
+          ]
+      );
     }
 }

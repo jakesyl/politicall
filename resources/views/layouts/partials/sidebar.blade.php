3cecf7a9de1d1233a95c0c67748c4f624c9e2918
Jake Sylvestre <jakesyl@gmail.com>
2016-09-11 04:56:41 +0000

    add sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -35,16 +35,9 @@
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-tachometer'></i> <span>Overview</span></a></li>
             <li><a href="/calls"><i class='fa fa-phone'></i> <span>Calls</span></a></li>
-	    <li><a href="/calls"><i class='fa fa-users'></i> <span>Contacts</span></a></li>
+	    <li><a href="/contacts"><i class='fa fa-users'></i> <span>Contacts</span></a></li>
 	    
-            <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
-                <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                </ul>
-            </li>
-        </ul><!-- /.sidebar-menu -->
+            </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
 </aside>
